0d12a264406289456e6f560d91f832479d42b1c8
Federico Aloi <federico.aloi@gmail.com>
2014-07-03 04:47:20 -0300

    Initial commit

---
+++ Converter/Domain/MilesConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.Domain
+{
+	public class MilesConverter : INotifyPropertyChanged
+	{
+		private double kilometres;
+		public double Miles { get; set; }
+
+		public double Kilometres
+		{
+			get { return kilometres; }
+			set { kilometres = value; OnPropertyChanged(); }
+		}
+
+		public void Convert()
+		{
+			this.Kilometres = this.Miles * 1.60934;
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			var handler = PropertyChanged;
+			if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ Converter/UI/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Converter.Domain;
+
+namespace Converter.UI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MilesConverter Model { get { return (this.DataContext as MilesConverter); } }
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.DataContext = new MilesConverter { Miles = 100 };
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			this.Model.Convert();
+		}
+	}
+}
